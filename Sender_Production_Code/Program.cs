b198a40b4221d84b16b29072ce45124e653408eb
Ananda Krishnamurthy (SX/EDC2) <act8kor@bosch.com>
2022-10-28 19:05:30 +0530

    File Update

---
+++ Sender_Production_Code/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Sender_Production_Code
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Sender_Production_Code.Console_Output Instance_Console_Output = new Sender_Production_Code.Console_Output();
+
+            Instance_Console_Output.Print_Sender_Data();
+        }
+    }
+}
